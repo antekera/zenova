8d783941b706bf6ddd962e2175794c89c5bdf9ce
Miguel Antequera <miguel.antequera@maritz.com>
2023-07-31 17:13:05 -0400

    fix: node 18

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,4 @@ export { Footer } from './Footer';
 export { Icon } from './Icon';
 export { Logo } from './Logo';
 export { LogoText } from './LogoText';
-// eslint-disable-next-line import/no-cycle
 export { Menu } from './Menu';
